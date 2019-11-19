b2316d69c20b9a2a07e40a11c03cb882737f4fb1
Patrícia Castro <patricia.castro@fabamaq.com>
2019-11-19 10:13:40 +0000

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-layout>
+      <h1>Home page</h1>
+    </v-layout>
+    <Login class="mt-5" />
+  </v-container>
+</template>
+
+<script>
+
+  import Login from "../components/Login";
+
+  export default {
+    components: {
+      Login
+    }
+  };
+</script>
